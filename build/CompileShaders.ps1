7832fea8aebadb4fae66fbb6bbcfad1923f6040a
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-09-20 21:48:29 +0100

    Moved ensuring the output folder out of the loop

--- build/CompileShaders.ps1
+++ build/CompileShaders.ps1
@@ -6,11 +6,11 @@ param(
 $Types = ('.frag', '.vert')
 
 $Files = Get-ChildItem "$ProjectDir\shaders\*" | Where-Object { $_.extension -in $Types }
+New-Item "$OutDir\shaders" -ItemType Directory
 
 foreach ($file in $Files) {
 	$fileName = [System.IO.Path]::GetFilenameWithoutExtension($file)
 	Write-Host "Compiling $($file.name)..."
-	New-Item "$OutDir\shaders" -ItemType Directory
 	glslc $file -o "$($OutDir)shaders\$($fileName).spv"
 	Write-Host "Compilation complete"
 }
